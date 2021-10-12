154b365f28f9e4cdba9ee46033c0093132c31a0a
Miika Härmä <harmamiika@gmail.com>
2021-10-12 11:47:54 +0300

    Updates

---
+++ precache-manifest.07d1ab6c1d5a6e30ff73e1a08678db90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863038e2eb4430ba7428a6e07f5e0377",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac95a7e1016f27cec8b9",
+    "url": "./static/css/2.e45429e2.chunk.css"
+  },
+  {
+    "revision": "bd1894e67ca27e925f1b",
+    "url": "./static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "ac95a7e1016f27cec8b9",
+    "url": "./static/js/2.b26bdda1.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.b26bdda1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd1894e67ca27e925f1b",
+    "url": "./static/js/main.3678afff.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
